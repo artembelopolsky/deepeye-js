929a38f85ae9ce04aa06cb33a6de40e2e5830759
Laurens Stuurman <l.w.a.sturman@vu.nl>
2023-10-23 11:46:22 +0200

    renamed bundle file to deepeyetracker.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: './src/eyetracker.js',
   output: {
-    filename: 'main.js',
+    filename: 'deepeyetracker.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
